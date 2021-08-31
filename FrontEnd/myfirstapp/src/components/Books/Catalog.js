cdb1d6860cea2b402791c9aeddfa4a82fc66325e
David Nguyen <s3843540@student.rmit.edu.au>
2021-08-31 15:05:48 +1000

    added some frontend files

---
+++ FrontEnd/myfirstapp/src/components/Books/Catalog.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Catalog extends Component {
+    render() {
+        return (
+            <div className="container">
+                this is the catalog page
+            </div>
+        )
+    }
+}
+export default Catalog;
