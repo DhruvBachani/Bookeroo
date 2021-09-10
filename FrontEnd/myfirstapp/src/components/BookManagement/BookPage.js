e670c684394121f03eed15cc264b9ce683547461
David Nguyen <s3843540@student.rmit.edu.au>
2021-09-10 17:26:09 +1000

    added book cover image code

--- FrontEnd/myfirstapp/src/components/BookManagement/BookPage.js
+++ FrontEnd/myfirstapp/src/components/BookManagement/BookPage.js
@@ -6,8 +6,14 @@ class BookPage extends Component {
       <div className="bookCatalog">
         <div className="light-overlay landing-inner text-dark">
           <div className="container">
-            <div className="col-md-12 text-center">
-              This is the book page
+            <div className="row">
+              <div className="col-md-12">
+                <div className="book-cover-image-container">
+                  <img src="" alt="Image not found" onError={
+                    (e)=>{e.target.onerror = null; e.target.src="/default.jpg"}
+                  }/>
+                </div>
+              </div>
             </div>
           </div>
         </div>
