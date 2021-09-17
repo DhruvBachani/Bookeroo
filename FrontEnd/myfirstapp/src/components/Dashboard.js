46cb57dd6a84b460832adf5da6d90b3c38d50b60
Dhruv <s3808392@student.rmit.edu.au>
2021-09-17 17:55:35 +1000

    Landing page redirected & table auto creation set

--- FrontEnd/myfirstapp/src/components/Dashboard.js
+++ FrontEnd/myfirstapp/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import {logout} from "../actions/securityActions";
 import CreateRequestButton from "./UserManagement/CreateRequestButton";
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 
 class Dashboard extends Component {
@@ -34,6 +35,10 @@ class Dashboard extends Component {
                 <div className="row">
                     <div className="col-md-12">
                         <h1 className="display-4 text-left">Dashboard</h1>
+                        <br />
+                        <Link className="btn btn-lg btn-secondary mr-2" to="/newAd">
+                            Post an ad
+                        </Link>
                         <br />
                         {(this.props.security.user.userType === "Admin") && <CreateRequestButton />}
                         <hr />
