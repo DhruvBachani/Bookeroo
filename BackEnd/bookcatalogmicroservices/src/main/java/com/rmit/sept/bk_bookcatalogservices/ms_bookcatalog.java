cccfcd3fa6ba5d31b42cc1c1a80921bc4044efcb
Dhruv <s3808392@student.rmit.edu.au>
2021-09-21 21:03:18 +1000

    minor bug fix

--- BackEnd/bookcatalogmicroservices/src/main/java/com/rmit/sept/bk_bookcatalogservices/ms_bookcatalog.java
+++ BackEnd/bookcatalogmicroservices/src/main/java/com/rmit/sept/bk_bookcatalogservices/ms_bookcatalog.java
@@ -2,13 +2,12 @@ package com.rmit.sept.bk_bookcatalogservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.testng.annotations.Test;
+
 
 @SpringBootApplication
 public class ms_bookcatalog {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(ms_login.class, args);
     }
-
 }
